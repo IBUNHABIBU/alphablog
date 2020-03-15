93c89d3c45b02c1a56ed2479d28a4cdbab01cbf4
IBUNHABIBU <ibunhabib@gmail.com>
2020-03-15 09:46:49 +0000

    testing delete feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,7 +21,26 @@ class ArticlesController < ApplicationController
   def show 
     @article = Article.find(params[:id])
   end
-  
+  def edit 
+     @article = Article.find(params[:id])
+  end
+  def update 
+    @article = Article.find(params[:id])
+    if @article.update(article_params) 
+      flash[:success]= "Article has been updated"
+      redirect_to @article 
+    else
+      flash.now[:danger]="Article has not been updated"
+      render :edit
+    end
+  end
+  def destroy 
+     @article = Article.find(params[:id])
+     if @article.destroy 
+       flash[:success]= "Article has been deleted"
+        redirect_to articles_path
+     end
+  end
   protected 
   def resource_not_found
     message = "The article you are looking for could not be found"
