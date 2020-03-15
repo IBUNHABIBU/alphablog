c685c7c8c246fe4aedec2d4f1d6230dd19c979a4
IBUNHABIBU <ibunhabib@gmail.com>
2020-03-15 09:57:26 +0000

    refactoring

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class ArticlesController < ApplicationController
+  
+before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def index
     @article = Article.all
   end
@@ -19,13 +22,10 @@ class ArticlesController < ApplicationController
     end
   end
   def show 
-    @article = Article.find(params[:id])
   end
   def edit 
-     @article = Article.find(params[:id])
   end
   def update 
-    @article = Article.find(params[:id])
     if @article.update(article_params) 
       flash[:success]= "Article has been updated"
       redirect_to @article 
@@ -35,7 +35,6 @@ class ArticlesController < ApplicationController
     end
   end
   def destroy 
-     @article = Article.find(params[:id])
      if @article.destroy 
        flash[:success]= "Article has been deleted"
         redirect_to articles_path
@@ -48,6 +47,9 @@ class ArticlesController < ApplicationController
     redirect_to root_path
   end
   private 
+    def set_article
+      @article = Article.find(params[:id])
+    end
     def article_params
       params.require(:article).permit(:title,:body)
     end
