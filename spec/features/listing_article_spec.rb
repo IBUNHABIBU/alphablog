62695c4f20a7ccf85ad1b558b60cfc22c7f08fa7
IBUNHABIBU <ibunhabib@gmail.com>
2020-03-16 16:01:52 +0000

    albe to create article when signed in

--- spec/features/listing_article_spec.rb
+++ spec/features/listing_article_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 RSpec.feature "Listing Articles" do 
  before do 
-   john = User.create(email:"john@gmail.com",password:"123456")
-  @article1 = Article.create(title:"Article heading 1",body:"Body of first article",user: john)
-  @article2 = Article.create(title:"Title for second article",body:"This is the body of second article",user: john)
+   @john = User.create(email:"john@gmail.com",password:"123456")
+  @article1 = Article.create(title:"Article heading 1",body:"Body of first article",user: @john)
+  @article2 = Article.create(title:"Title for second article",body:"This is the body of second article",user: @john)
  end
  scenario "A user list all articles" do 
   visit '/'
@@ -24,6 +24,19 @@ RSpec.feature "Listing Articles" do
   expect(page).to have_link(@article1.title)
   expect(page).to have_link(@article2.title)
   expect(page).not_to have_link("New Article")
+ end
+ 
+ scenario "with article created and user  signed in " do 
+  login_as(@john)
+  visit '/'
+  
+  expect(page).to have_content(@article1.title)
+  expect(page).to have_content(@article1.body)
+  expect(page).to have_content(@article2.title)
+  expect(page).to have_content(@article2.body)
+  expect(page).to have_link(@article1.title)
+  expect(page).to have_link(@article2.title)
+  expect(page).to have_link("New Article")
  end
  
  scenario "A user has no articles" do 
